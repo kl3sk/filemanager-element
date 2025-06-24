891f27215ea33b76a0873b5106547ff13f623b37
klesk <klesk44@gmail.com>
2025-06-24 15:30:10 +0200

    Adds close button to the sidebar
    Implements a close button on the sidebar.
    
    The button dispatches a custom event to allow the parent component
    to handle the closing of the sidebar.

--- src/ui/Sidebar/Sidebar.svelte
+++ src/ui/Sidebar/Sidebar.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
   import Folders from "./Folders.svelte";
   import Search from "./Search.svelte";
-
+  import IconDelete from '../icons/IconDelete.svelte'
+  import { onMount } from 'svelte';
   export let lazyFolders: boolean
+
+  let componentEl: HTMLElement;
+
+  function handleClose() {
+    const event = new CustomEvent('close', {
+      bubbles: true,
+      composed: true
+    });
+
+    // Dispatch depuis l’élément racine du composant
+    componentEl.dispatchEvent(event);
+  }
 </script>
 
 <template>
-  <aside class="fm-sidebar">
+  <aside class="fm-sidebar" bind:this={componentEl}>
+    <div class="icon-close" on:click={handleClose}>
+        <IconDelete />
+    </div>
     <Search />
     <Folders folders={[null]} lazyLoad={lazyFolders} />
   </aside>
@@ -24,5 +40,19 @@
     height: 1px;
     background-color: var(--fm-border);
     margin: 1.5em 0;
+  }
+
+  .icon-close
+  {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    padding: 16px;
+    color: var(--fm-color);
+    transition: .3s color;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
   }
 </style>
