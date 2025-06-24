ed31d9dd2dd686e4de9febe45c99488615ef6589
klesk <klesk44@gmail.com>
2025-06-24 16:43:38 +0200

    Adds file upload functionality
    Implements a file upload feature to allow users to upload files with a button.
    
    It dynamically creates a file input element and handles the selection and uploading of files.
    
    Option have to be added for:
    - Is multiple
    - Allowed file types

--- src/ui/Sidebar/Sidebar.svelte
+++ src/ui/Sidebar/Sidebar.svelte
@@ -1,59 +1,76 @@
 <script lang="ts">
-  import Folders from "./Folders.svelte";
-  import Search from "./Search.svelte";
-  import IconDelete from '../icons/IconDelete.svelte'
-  import IconUpload from "../icons/IconUpload.svelte"
-  export let lazyFolders: boolean
+    import Folders from "./Folders.svelte"
+    import Search from "./Search.svelte"
+    import IconUpload from "../icons/IconUpload.svelte"
 
-  let componentEl: HTMLElement;
+    import { useQueryClient } from "../../query";
+    import { uploadFile, folder, getOptions, uploads } from '../../store';
 
-  function handleClose() {
-    const event = new CustomEvent('close', {
-      bubbles: true,
-      composed: true
-    });
+    export let lazyFolders: boolean
 
-    // Dispatch depuis l’élément racine du composant
-    componentEl.dispatchEvent(event);
-  }
+    const queryClient = useQueryClient();
+    const options = getOptions()
+
+    function handleUpload() {
+        // Crée dynamiquement un input de type file
+        const input = document.createElement('input') as HTMLInputElement
+        input.type = 'file'
+        input.multiple = true
+        input.accept = '*/*' // Tu peux filtrer, ex: 'image/*', 'application/pdf', etc.
+
+
+        // Ajoute un gestionnaire d'événement pour le fichier sélectionné
+        input.onchange = function (event) {
+          const target = event.target;
+
+          if (target instanceof HTMLInputElement && target.files) {
+            Array.from(target.files as FileList).forEach(async (file: File) => {
+              uploads.push(file)
+              await uploadFile(options, queryClient, file, $folder)
+              uploads.remove(file)
+            });
+          }
+        }
+
+        // Déclenche la boîte de dialogue
+        input.click()
+    }
 </script>
 
 <template>
-  <aside class="fm-sidebar" bind:this={componentEl}>
+  <aside class="fm-sidebar">
     <div class="icons">
-      <span class="close" on:click={handleClose}>
-        <IconDelete on:click={handleClose}/>
+      <span class="upload" on:click={handleUpload}>
+        <IconUpload/>
       </span>
     </div>
-    <Search />
-    <Folders folders={[null]} lazyLoad={lazyFolders} />
+    <Search/>
+    <Folders folders={[null]} lazyLoad={lazyFolders}/>
   </aside>
 </template>
 
 <style>
-  .fm-sidebar {
-    border-right: 1px solid var(--fm-border);
-    padding: 24px;
-    overflow: auto;
-  }
-  .fm-sidebar > :global(*) + :global(*)::before {
-    content: "";
-    display: block;
-    height: 1px;
-    background-color: var(--fm-border);
-    margin: 1.5em 0;
-  }
+    .fm-sidebar {
+        border-right: 1px solid var(--fm-border);
+        padding: 24px;
+        overflow: auto;
+    }
 
-  .icons
-  {
-    display: flex;
-    justify-content: space-between;
-  }
+    .fm-sidebar > :global(*) + :global(*)::before {
+        content: "";
+        display: block;
+        height: 1px;
+        background-color: var(--fm-border);
+        margin: 1.5em 0;
+    }
 
-  .close
-  {
-    color: var(--fm-color);
-    border: none;
-    cursor: pointer;
-  }
+    .icons {
+        display: flex;
+        justify-content: end; /* Can be space-between if "close PR" is merged */
+    }
+
+    .upload {
+        color: var(--fm-color);
+        cursor: pointer;
+    }
 </style>
